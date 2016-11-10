a0bc295997e9bd5f2c9714eee5c8591fb89a1be4
promotor <promotor@note-desur-01>
2016-11-10 15:59:53 -0300

    Novo metodo de busca de CEP atraves de JSON

--- SisAcademic/src/br/com/gfsoft/sisacademic/util/BuscaCep.java
+++ SisAcademic/src/br/com/gfsoft/sisacademic/util/BuscaCep.java
@@ -1,8 +1,15 @@
 package br.com.gfsoft.sisacademic.util;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
+import java.net.URL;
 import java.text.ParseException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
@@ -13,74 +20,162 @@ import org.jsoup.select.Elements;
 public class BuscaCep {
 
 	public String getLogradouro(String CEP) throws IOException {
-
+		
+		JSONObject jsonObjeto;
+		JSONParser parser = new JSONParser();
+				
 		try {
-
-			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
-			Elements urlPesquisa = doc.select("span[itemprop=streetAddress]");
-			for (Element urlEndereco : urlPesquisa) {
-				return urlEndereco.text();
-			}
-
-		} catch (SocketTimeoutException e) {
-			System.out.println(e);
-		} catch (HttpStatusException w) {
-			System.out.println(w);
+			
+			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
+			Reader reader = new InputStreamReader(url.openStream());
+			jsonObjeto = (JSONObject) parser.parse(reader);
+			
+			return (String) jsonObjeto.get("logradouro");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+//		try {
+//
+//			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
+//			Elements urlPesquisa = doc.select("span[itemprop=streetAddress]");
+//			for (Element urlEndereco : urlPesquisa) {
+//				return urlEndereco.text();
+//			}
+//
+//		} catch (SocketTimeoutException e) {
+//			System.out.println(e);
+//		} catch (HttpStatusException w) {
+//			System.out.println(w);
+//		}
 		return "";
 	}
 
 	public String getBairro(String CEP) throws IOException {
-
+		
+		JSONObject jsonObjeto;
+		JSONParser parser = new JSONParser();
+				
 		try {
-
-			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
-			Elements urlPesquisa = doc.select("td:gt(1)");
-			for (Element urlBairro : urlPesquisa) {
-				return urlBairro.text();
-			}
-
-		} catch (SocketTimeoutException e) {
-			System.out.println(e);
-		} catch (HttpStatusException w) {
-			System.out.println(w);
+			
+			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
+			Reader reader = new InputStreamReader(url.openStream());
+			jsonObjeto = (JSONObject) parser.parse(reader);
+			
+			return (String) jsonObjeto.get("bairro");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+//		try {
+//
+//			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
+//			Elements urlPesquisa = doc.select("td:gt(1)");
+//			for (Element urlBairro : urlPesquisa) {
+//				return urlBairro.text();
+//			}
+//
+//		} catch (SocketTimeoutException e) {
+//			System.out.println(e);
+//		} catch (HttpStatusException w) {
+//			System.out.println(w);
+//		}
 		return "";
 	}
 
 	public String getCidade(String CEP) throws IOException {
-
+		
+		JSONObject jsonObjeto;
+		JSONParser parser = new JSONParser();
+				
 		try {
-
-			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
-			Elements urlPesquisa = doc.select("span[itemprop=addressLocality]");
-			for (Element urlCidade : urlPesquisa) {
-				return urlCidade.text();
-			}
-
-		} catch (SocketTimeoutException e) {
-			System.out.println(e);
-		} catch (HttpStatusException w) {
-			System.out.println(w);
+			
+			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
+			Reader reader = new InputStreamReader(url.openStream());
+			jsonObjeto = (JSONObject) parser.parse(reader);
+			
+			return (String) jsonObjeto.get("cidade");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+//		try {
+//
+//			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
+//			Elements urlPesquisa = doc.select("span[itemprop=addressLocality]");
+//			for (Element urlCidade : urlPesquisa) {
+//				return urlCidade.text();
+//			}
+//
+//		} catch (SocketTimeoutException e) {
+//			System.out.println(e);
+//		} catch (HttpStatusException w) {
+//			System.out.println(w);
+//		}
 		return "";
 	}
 
 	public String getEstado(String CEP) throws IOException {
-
+		
+		JSONObject jsonObjeto;
+		JSONParser parser = new JSONParser();
+				
 		try {
-
-			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
-			Elements urlPesquisa = doc.select("span[itemprop=addressRegion]");
-			for (Element urlEstado : urlPesquisa) {
-				return urlEstado.text();
-			}
-
-		} catch (SocketTimeoutException e) {
-			System.out.println(e);
-		} catch (HttpStatusException w) {
-			System.out.println(w);
+			
+			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
+			Reader reader = new InputStreamReader(url.openStream());
+			jsonObjeto = (JSONObject) parser.parse(reader);
+			
+			return (String) jsonObjeto.get("estado");
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+//		try {
+//
+//			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
+//			Elements urlPesquisa = doc.select("span[itemprop=addressRegion]");
+//			for (Element urlEstado : urlPesquisa) {
+//				return urlEstado.text();
+//			}
+//
+//		} catch (SocketTimeoutException e) {
+//			System.out.println(e);
+//		} catch (HttpStatusException w) {
+//			System.out.println(w);
+//		}
 		return "";
 	}
 
