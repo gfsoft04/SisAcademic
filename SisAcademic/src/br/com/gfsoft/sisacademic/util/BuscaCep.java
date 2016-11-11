145aea6b4cd9ce976b50cb4a9b50ffb38713df66
promotor <promotor@note-desur-01>
2016-11-11 09:35:59 -0300

    Busca endereco pelo cep

--- SisAcademic/src/br/com/gfsoft/sisacademic/util/BuscaCep.java
+++ SisAcademic/src/br/com/gfsoft/sisacademic/util/BuscaCep.java
@@ -4,17 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 import java.net.URL;
-import java.text.ParseException;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.jsoup.HttpStatusException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import org.json.simple.parser.ParseException;
 
 import br.com.gfsoft.sisacademic.model.Endereco;
 
@@ -22,32 +16,44 @@ public class BuscaCep {
 	
 	private Endereco endereco;
 	private JSONObject jsonObjeto;
-	JSONParser parser;
-
-	public String getLogradouro(String CEP) throws IOException {
-		
-		
+	private JSONParser parser;
+	private URL url;
+	private Reader reader;
+	
+	public Endereco getEndereco(String cep){
 		parser = new JSONParser();
-				
+		endereco = new Endereco();
 		try {
-			
-			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
-			Reader reader = new InputStreamReader(url.openStream());
+			url = new URL("http://api.postmon.com.br/v1/cep/" + cep);
+			reader = new InputStreamReader(url.openStream());
 			jsonObjeto = (JSONObject) parser.parse(reader);
 			
-			return (String) jsonObjeto.get("logradouro");
+			endereco.setLogradouro((String) jsonObjeto.get("logradouro"));
+			endereco.setBairro((String) jsonObjeto.get("bairro"));
+			endereco.setCidade((String) jsonObjeto.get("cidade"));
+			endereco.setEstado((String) jsonObjeto.get("estado"));
+			
+			return endereco;
 			
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
+			System.out.println("Url mal formatada!");
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			System.out.println("Cep nao encontrado!");
 			e.printStackTrace();
-		} catch (org.json.simple.parser.ParseException e) {
+		} catch (ParseException e) {
 			// TODO Auto-generated catch block
+			System.out.println("ParseExcepition");
 			e.printStackTrace();
 		}
+		
+		return null;
+	}
 
+//	public String getLogradouro(String CEP) throws IOException {
+//
 //		try {
 //
 //			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
@@ -61,33 +67,11 @@ public class BuscaCep {
 //		} catch (HttpStatusException w) {
 //			System.out.println(w);
 //		}
-		return "";
-	}
-
-	public String getBairro(String CEP) throws IOException {
-		
-		JSONObject jsonObjeto;
-		JSONParser parser = new JSONParser();
-				
-		try {
-			
-			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
-			Reader reader = new InputStreamReader(url.openStream());
-			jsonObjeto = (JSONObject) parser.parse(reader);
-			
-			return (String) jsonObjeto.get("bairro");
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (org.json.simple.parser.ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+//		return "";
+//	}
+//
+//	public String getBairro(String CEP) throws IOException {
+//		
 //		try {
 //
 //			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
@@ -101,33 +85,11 @@ public class BuscaCep {
 //		} catch (HttpStatusException w) {
 //			System.out.println(w);
 //		}
-		return "";
-	}
-
-	public String getCidade(String CEP) throws IOException {
-		
-		JSONObject jsonObjeto;
-		JSONParser parser = new JSONParser();
-				
-		try {
-			
-			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
-			Reader reader = new InputStreamReader(url.openStream());
-			jsonObjeto = (JSONObject) parser.parse(reader);
-			
-			return (String) jsonObjeto.get("cidade");
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (org.json.simple.parser.ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+//		return "";
+//	}
+//
+//	public String getCidade(String CEP) throws IOException {
+//		
 //		try {
 //
 //			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
@@ -141,33 +103,11 @@ public class BuscaCep {
 //		} catch (HttpStatusException w) {
 //			System.out.println(w);
 //		}
-		return "";
-	}
-
-	public String getEstado(String CEP) throws IOException {
-		
-		JSONObject jsonObjeto;
-		JSONParser parser = new JSONParser();
-				
-		try {
-			
-			URL url = new URL("http://api.postmon.com.br/v1/cep/" + CEP);
-			Reader reader = new InputStreamReader(url.openStream());
-			jsonObjeto = (JSONObject) parser.parse(reader);
-			
-			return (String) jsonObjeto.get("estado");
-			
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (org.json.simple.parser.ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+//		return "";
+//	}
+//
+//	public String getEstado(String CEP) throws IOException {
+//		
 //		try {
 //
 //			Document doc = Jsoup.connect("http://www.qualocep.com/busca-cep/" + CEP).timeout(120000).get();
@@ -181,47 +121,47 @@ public class BuscaCep {
 //		} catch (HttpStatusException w) {
 //			System.out.println(w);
 //		}
-		return "";
-	}
-
-	public String getLatLong(String CEP) throws IOException, ParseException {
-
-		try {
-
-			if (CEP.contains("-")) {
-				Document doc = Jsoup.connect("http://maps.googleapis.com/maps/api/geocode/xml?address=" + CEP
-						+ "&language=pt-BR&sensor=true").timeout(120000).get();
-				Elements lat = doc.select("geometry").select("location").select("lat");
-				Elements lng = doc.select("geometry").select("location").select("lng");
-				for (Element Latitude : lat) {
-					for (Element Longitude : lng) {
-						return Latitude.text() + "," + Longitude.text();
-					}
-
-				}
-			} else {
-
-				StringBuilder cepHif = new StringBuilder(CEP);
-				cepHif.insert(CEP.length() - 3, '-');
-
-				Document doc = Jsoup.connect("http://maps.googleapis.com/maps/api/geocode/xml?address=" + cepHif
-						+ "&language=pt-BR&sensor=true").timeout(120000).get();
-				Elements lat = doc.select("geometry").select("location").select("lat");
-				Elements lng = doc.select("geometry").select("location").select("lng");
-				for (Element Latitude : lat) {
-					for (Element Longitude : lng) {
-						return Latitude.text() + "," + Longitude.text();
-					}
-
-				}
-			}
-
-		} catch (SocketTimeoutException e) {
-
-		} catch (HttpStatusException w) {
-
-		}
-		return "";
-	}
+//		return "";
+//	}
+//
+//	public String getLatLong(String CEP) throws IOException, ParseException {
+//
+//		try {
+//
+//			if (CEP.contains("-")) {
+//				Document doc = Jsoup.connect("http://maps.googleapis.com/maps/api/geocode/xml?address=" + CEP
+//						+ "&language=pt-BR&sensor=true").timeout(120000).get();
+//				Elements lat = doc.select("geometry").select("location").select("lat");
+//				Elements lng = doc.select("geometry").select("location").select("lng");
+//				for (Element Latitude : lat) {
+//					for (Element Longitude : lng) {
+//						return Latitude.text() + "," + Longitude.text();
+//					}
+//
+//				}
+//			} else {
+//
+//				StringBuilder cepHif = new StringBuilder(CEP);
+//				cepHif.insert(CEP.length() - 3, '-');
+//
+//				Document doc = Jsoup.connect("http://maps.googleapis.com/maps/api/geocode/xml?address=" + cepHif
+//						+ "&language=pt-BR&sensor=true").timeout(120000).get();
+//				Elements lat = doc.select("geometry").select("location").select("lat");
+//				Elements lng = doc.select("geometry").select("location").select("lng");
+//				for (Element Latitude : lat) {
+//					for (Element Longitude : lng) {
+//						return Latitude.text() + "," + Longitude.text();
+//					}
+//
+//				}
+//			}
+//
+//		} catch (SocketTimeoutException e) {
+//
+//		} catch (HttpStatusException w) {
+//
+//		}
+//		return "";
+//	}
 
 }
