e5812e5f460c17d750991c15fdcaae3663a2fab7
FelipeErmeson <felipeemersson@gmail.com>
2016-11-23 13:20:06 -0300

    academico e persistence

--- SisAcademic/src/br/com/gfsoft/sisacademic/controller/Academico.java
+++ SisAcademic/src/br/com/gfsoft/sisacademic/controller/Academico.java
@@ -1,9 +1,12 @@
 package br.com.gfsoft.sisacademic.controller;
 
 import java.util.List;
+import java.util.Set;
 
 import br.com.gfsoft.sisacademic.model.Aluno;
 import br.com.gfsoft.sisacademic.model.Disciplina;
+import br.com.gfsoft.sisacademic.model.Funcionario;
+import br.com.gfsoft.sisacademic.model.Professor;
 import br.com.gfsoft.sisacademic.service.AlunoService;
 import br.com.gfsoft.sisacademic.service.DisciplinaService;
 import br.com.gfsoft.sisacademic.service.FuncionarioService;
@@ -25,6 +28,8 @@ public class Academico {
 		this.funcionarioService = new FuncionarioService();
 	}
 	
+	//-------------Metodos da Disciplina------------------------
+	
 	public boolean cadastrarDisciplina(Disciplina disciplina){
 		return disciplinaService.cadastrar(disciplina);
 	}
@@ -41,14 +46,74 @@ public class Academico {
 		return disciplinaService.buscar(id);
 	}
 	
-	public List<Disciplina> listarDisciplina(){
+	public List<Disciplina> listarDisciplinas(){
 		return disciplinaService.listar();
 	}
 	
-	//-------------------------------------------------
+	//-------------Metodos do Aluno-----------------------------
 	
 	public boolean cadastrarAluno(Aluno aluno){
 		return alunoService.cadastrar(aluno);
+	}
+	
+	public boolean deletarAluno(Aluno aluno){
+		return alunoService.deletar(aluno);
+	}
+	
+	public boolean atualizarAluno(Aluno aluno){
+		return alunoService.atualizar(aluno);
+	}
+	
+	public Aluno buscarAluno(String matricula){
+		return alunoService.buscar(matricula);
+	}
+	
+	public Set<Aluno> listarAlunos(){
+		return alunoService.listar();
+	}
+	
+	//-------------Metodos do Funcionario-----------------------------
+	
+	public boolean cadastrarFuncionario(Funcionario funcionario){
+		return funcionarioService.cadastrar(funcionario);
+	}
+	
+	public boolean deletarAluno(Funcionario funcionario){
+		return funcionarioService.deletar(funcionario);
+	}
+	
+	public boolean atualizarAluno(Funcionario funcionario){
+		return funcionarioService.atualizar(funcionario);
+	}
+	
+	public Funcionario buscarFuncionario(String matricula){
+		return funcionarioService.buscar(matricula);
+	}
+	
+	public Set<Funcionario> listarFuncionarios(){
+		return funcionarioService.listar();
+	}
+	
+	//-------------Metodos do Funcionario-----------------------------
+	
+	public boolean cadastrarProfessor(Professor professor){
+		return professorService.cadastrar(professor);
+	}
+	
+	public boolean deletarProfessor(Professor professor){
+		return professorService.deletar(professor);
+	}
+	
+	public boolean atualizarProfessor(Professor professor){
+		return professorService.atualizar(professor);
+	}
+	
+	public Professor buscarProfessor(String matricula){
+		return professorService.buscar(matricula);
+	}
+	
+	public Set<Professor> listarProfessores(){
+		return professorService.listar();
 	}
 	
 }
--- SisAcademic/src/br/com/gfsoft/sisacademic/persistence/PersistenceProfessor.java
+++ SisAcademic/src/br/com/gfsoft/sisacademic/persistence/PersistenceProfessor.java
@@ -1,14 +1,39 @@
 package br.com.gfsoft.sisacademic.persistence;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Set;
 
 import br.com.gfsoft.sisacademic.model.Professor;
 
 public class PersistenceProfessor implements IPersistenceProfessor {
+	
+	private static Statement stmt;
+	private static ResultSet rs;
+	private static Conexao con = new Conexao();
 
 	@Override
 	public boolean insert(Professor professor) {
-		// TODO Auto-generated method stub
+		
+		/*
+		PersistenceFuncionario pFuncionario = new PersistenceFuncionario();
+		
+		String sql;
+		long id;
+		
+		if(pFuncionario.insert(professor)){
+			
+			id = pFuncionario.selectFuncionario(professor.getMatricula());
+			
+			sql = "INSERT INTO tb_Professor(tb_Funcionario_idPessoa, titularidade) VALUES "
+					+ "("+id+", '"+professor.getTitularidade()+"')";
+			
+			con.getConnection().createStatement().executeUpdate(sql);
+			
+			
+		} */
+	
+		
 		return false;
 	}
 
