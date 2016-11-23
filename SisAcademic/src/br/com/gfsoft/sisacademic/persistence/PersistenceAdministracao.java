cc5ddec780e442108c5b579ae2f0d13fa042cd52
Prismatiic <matheusaraujo1995m@gmail.com>
2016-11-23 18:29:44 -0300

    Fiz modificações em PersistenceAdministracao. Faltando apenas update , select e set.

--- SisAcademic/src/br/com/gfsoft/sisacademic/persistence/PersistenceAdministracao.java
+++ SisAcademic/src/br/com/gfsoft/sisacademic/persistence/PersistenceAdministracao.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import javax.swing.JOptionPane;
 
 import br.com.gfsoft.sisacademic.model.Administracao;
+import br.com.gfsoft.sisacademic.model.Funcionario;
 
 public class PersistenceAdministracao implements IPersistenceAdministracao {
 
@@ -18,35 +19,68 @@ public class PersistenceAdministracao implements IPersistenceAdministracao {
 	@Override
 	public boolean insert(Administracao administracao) {
 		
-		//PersistenceFuncionario pFuncionario = new PersistenceFuncionario();
-		//String sql;
-		//long id;
+		PersistenceFuncionario pFuncionario = new PersistenceFuncionario();
+		Funcionario funcionario = new Funcionario ();
 		
-//		if(pFuncionario.insert(administracao)){
-//			//id = pFuncionario.selectFuncionario(administracao.getMatricula());
-//			
-//		//	sql = "INSERT INTO tb_Aluno(tb_Funcionario_idPessoa, senha) "
-//	//				+ "VALUES("+ id + ","
-//		//			+ "'"+ administracao.getSenha() + "')";
-//			
-//			try {
-//				con.getConnection().createStatement().executeUpdate(sql);
-//				return true;
-//			} catch (SQLException ex) {
-//				// Excecao para banco de dados
-//				ex.printStackTrace();
-//				JOptionPane.showMessageDialog(null, "Erro na insercao do administrador na base!", "Erro", JOptionPane.ERROR_MESSAGE);
-//			}
-//		}//if inseriu corretamente em pessoa
-//		
-//		return false;
+		String sql;
+		long id;
+		
+		if(pFuncionario.insert(administracao)){
+			funcionario = pFuncionario.selectFuncionario(administracao.getMatricula());
+			
+			id = funcionario.getId();
+			sql = "INSERT INTO tb_Administracao(tb_Funcionario_idPessoa, senha) "
+					+ "VALUES("+ id + ","
+					+ "'"+ administracao.getSenha() + "')";
+			
+			try {
+				con.getConnection().createStatement().executeUpdate(sql);
+				return true;
+			} catch (SQLException ex) {
+				// Excecao para banco de dados
+				ex.printStackTrace();
+				JOptionPane.showMessageDialog(null, "Erro na insercao do administrador na base!", "Erro", JOptionPane.ERROR_MESSAGE);
+			}
+		}//if inseriu corretamente em pessoa
 		
 		return false;
+		
 	}
 
 	@Override
 	public boolean delete(Administracao administracao) {
-		// TODO Auto-generated method stub
+		PersistenceFuncionario pFuncionario = new PersistenceFuncionario();
+		Funcionario funcionario = new Funcionario ();
+		
+		String sqlPessoa;
+		String sqlFuncionario;
+		String sqlAdministracao;
+
+		long id;
+		
+		if(pFuncionario.delete(administracao)){
+			funcionario = pFuncionario.selectFuncionario(administracao.getMatricula());
+			id = funcionario.getId();
+			
+			sqlAdministracao = "DELETE FROM tb_Administracao WHERE tb_Funcionario_tb_Pessoa_idPessoa "+ administracao.getId() +"";
+			sqlFuncionario = "DELETE FROM  tb_Funcionario WHERE tb_Pessoa_idPessoa" + administracao.getId()+"";
+			sqlPessoa = "DELETE FROM tb_Pessoa WHERE idPessoa" + administracao.getId() + "";
+			
+			try{
+				con.getConnection().createStatement().executeUpdate(sqlAdministracao);
+				con.getConnection().createStatement().executeUpdate(sqlFuncionario);
+				con.getConnection().createStatement().executeUpdate(sqlPessoa);
+				return true;
+			}catch (SQLException ex){
+				ex.printStackTrace();
+				JOptionPane.showMessageDialog(null, "Erro ao deletar administrador da base de dados","Erro", JOptionPane.ERROR_MESSAGE);
+			}
+			
+				
+			
+		}
+		
+		
 		return false;
 	}
 
