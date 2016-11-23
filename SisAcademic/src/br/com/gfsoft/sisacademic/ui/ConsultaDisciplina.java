ef0397b7c4e3303a06d5d1128dbceb5eb69b7cea
Bruno <Bruno@Lenovo-G470>
2016-11-23 10:37:57 -0300

    Correcao no parseException na conversao do idDisciplina

--- SisAcademic/src/br/com/gfsoft/sisacademic/ui/ConsultaDisciplina.java
+++ SisAcademic/src/br/com/gfsoft/sisacademic/ui/ConsultaDisciplina.java
@@ -80,7 +80,7 @@ public class ConsultaDisciplina extends JInternalFrame {
 			public void mouseClicked(MouseEvent e) {
 				
 				if(e.getClickCount() == 2){
-					long id = Long.parseLong((String) table.getValueAt(table.getSelectedRow(), 0));
+					long id = Long.parseLong(table.getValueAt(table.getSelectedRow(),0).toString());
 					PersistenceDisciplina pPersistenceDisciplina = new PersistenceDisciplina();
 					Disciplina disciplina = new Disciplina();
 					disciplina = pPersistenceDisciplina.selectDisciplina(id);
